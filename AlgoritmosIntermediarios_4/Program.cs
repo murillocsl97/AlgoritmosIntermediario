4d6d44eefee0ea73a734b824da9e4f810e672014
murillocsl <murillo.csl97@gmail.com>
2023-09-25 18:44:18 -0300

    Mudançca nome

--- AlgoritmosIntermediarios_4/Program.cs
+++ AlgoritmosIntermediarios_4/Program.cs
@@ -1,3 +1,20 @@
-﻿Int32 cont, soma;
+﻿// Faça um programa que leia um valor n, inteiro e positivo, calcule e mostre a seguinte soma: S = 1 + 1/2 + 1/3 + 1/4 + … + 1/n.
 
-Console.WriteLine("")
+Int32 numero, sequencia, final = 1;
+
+Console.WriteLine("Digite um número inteiro: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero == 0)
+{
+    Console.WriteLine("0! = 1");
+}
+else
+{
+
+    for (sequencia = 1; sequencia <= numero; sequencia++)
+    {
+        final +/= sequencia;
+    }
+    Console.WriteLine(numero + " ! = " + final);
+}
